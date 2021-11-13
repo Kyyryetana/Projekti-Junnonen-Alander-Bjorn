ec266937d0902646844953a1413a151136694e94
nikoalander1986 <niko.alander@hotmail.com>
2021-11-13 05:07:29 +0200

    muokattu HTML, CSS valmis, JS aloiteltu

---
+++ projekti-1A.js
@@ -0,0 +1,10 @@
+function tarkista() {
+  var kauttajatunnus = document.getElementsByName('kauttajatunnus').value;
+  var salasana = document.getElementsByName('salasana').value;
+  var checkBoksit = document.getElementsByName('onkoMatelijaa_vastaus');
+  var tekstiKentta = document.getElementsByName('lisatietoja').value;
+  var pudotusvalikko = document.getElementsByName('pudotusvalikko').value;
+  var radionapit = document.getElementsByName('montakoMatelijaa_vastaus');
+
+}
+//???
