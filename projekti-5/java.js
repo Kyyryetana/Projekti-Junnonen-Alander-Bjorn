15766c327341c528538e5876e86167bb14775004
nikoalander1986 <niko.alander@hotmail.com>
2022-01-18 12:15:43 +0200

    siivous

--- projekti-5/java.js
+++
@@ -1,8 +0,0 @@
-var korttiTaka = document.getElementsByClassName('card-back');
-var korttiEtu = document.getElementsByClassName('card-front');
-//
-function sekoita(korttiEtu)
-{
-    korttiEtu.sort(function(a, b){return 0.5 - Math.random()});
-    return korttiEtu;
-}
