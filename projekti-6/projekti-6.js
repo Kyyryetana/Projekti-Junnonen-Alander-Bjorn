9bc927493545a61657f4c5b1ed49af6da4678169
nikoalander1986 <niko.alander@hotmail.com>
2022-01-20 13:00:21 +0200

    JS tietojen nouto HTML:stä JavaScriptiin

---
+++ projekti-6/projekti-6.js
@@ -0,0 +1,10 @@
+function store() {
+  let avain = document.getElementById('avain').value;
+  let omistaja = document.getElementById('tili').value;
+  let pankki = document.getElementById('pankki').value;
+  let saldo = document.getElementById('saldo').value;
+  console.log(avain);
+  console.log(omistaja);
+  console.log(pankki);
+  console.log(saldo);
+}
