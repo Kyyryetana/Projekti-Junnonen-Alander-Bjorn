99b641d40bfebe5712d2c53bfeb4cf8537bc2ff7
jasminjunnonen <90385536+jasminjunnonen@users.noreply.github.com>
2022-02-05 02:34:34 +0200

    Kynän paksuus lisätty

--- omaProjekti/piirrustusOhjelma.js
+++ omaProjekti/piirrustusOhjelma.js
@@ -5,3 +5,8 @@ const ctx = canvas.getContext('2d');
 function poista(){
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
+
+//Kynän paksuuden säädin
+ctx.lineWidth = 3;
+const changeWidth = value => ctx.lineWidth = value;
+ctx.lineCap = "round";
